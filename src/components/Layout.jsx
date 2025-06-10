a7b3fadb68b3b96996f025cd7e01002aa55ba24e
giuseppecasaburi <enzobiondi0000@libero.it>
2025-06-10 15:28:12 +0200

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import HomePage from "../pages/HomePage";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function Layout() {
+    return (
+        <>
+            <Header/>
+            <HomePage/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Layout;
