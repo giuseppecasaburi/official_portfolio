74092eac0508ce16d54bdb7342fb1f5fe2a23bdc
giuseppecasaburi <enzobiondi0000@libero.it>
2025-06-12 16:18:21 +0200

    Aggiunta icone per contatto e CV nella sezione Hero; miglioramenti stilistici e responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,26 +1,27 @@
-import { FaArrowDown } from "react-icons/fa";
+import { FaArrowDown, FaRegFilePdf } from "react-icons/fa";
 import TextTyping from "./TextTyping";
+import { CiMail } from "react-icons/ci";
 
 function HeroSection() {
     return (
         <>
             <section id="hero-section">
                 <div id="container-section" className="md:flex">
-                    <div id="left" className="md:w-2/4 md:my-auto">
+                    <div id="left" className="md:my-auto">
                         <img src="../../img/logo.png" alt="" className="self-center"/>
                         <TextTyping />
-                        <div id="tag" className="md:py-5 self-center">
-                            <p className="rtw-button rounded-full md:my-2 ">
+                        <div id="tag" className="md:py-5">
+                            <span className="rtw-button ms-btn-d rounded-full md:my-1">
                                 <span id="disponibile"></span>
                                 Disponibile per lavorare
-                            </p>
+                            </span>
                         </div>
-                        <div id="button-area" className="md:mt-5 self-center">
-                            <a href="" className="illuminated-button md:me-3">Contattami</a>
-                            <a href="" className="illuminated-button md:ms-3">CV</a>
+                        <div id="button-area" className="md:mt-2">
+                            <a href="" className="illuminated-button ms-btn-d "><CiMail id="contact" size={20} className="inline-block "/> <span className="inline-block text-center">Contattami</span></a>
+                            <a href="" className="illuminated-button ms-btn-d md:ms-3"><FaRegFilePdf size={20} className="inline-block "/> <span className="inline-block text-center">CV</span></a>
                         </div>
                     </div>
-                    <div id="right" className="w-2/4 my-auto hidden md:inline-block">
+                    <div id="right" className="my-auto ">
                         <img src="../../img/pc_sfondo_1.png" alt="" />
                     </div>
                 </div>
